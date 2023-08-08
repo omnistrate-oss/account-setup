74ada5364d24311f225fc006f8be8da5d2d43ad8
Alok Nikhil <alok@omnistrate.com>
2023-08-07 18:17:44 -0700

    Add missing service account for the Config Connector

--- gcp/service_account.tf
+++ gcp/service_account.tf
@@ -21,12 +21,21 @@ data "google_project" "current" {
     project_id = var.project_id
 }
 
+# Omnistrate bootstrap service account
 resource "google_service_account" "omnistrate_bootstrap" {
   project = data.google_project.current.project_id
   account_id   = "omnistrate-bootstrap"
   display_name = "Omnistrate Bootstrap Service Account"
 }
 
+# Config connector bootstrap service account
+resource "google_service_account" "config_connector_bootstrap" {
+  project = data.google_project.current.project_id
+  account_id   = "config-connector-sa"
+  display_name = "Config Connector Bootstrap Service Account"
+}
+
+## Perms for omnistrate bootstrap service account
 resource "google_project_iam_member" "compute_admin" {
   project = data.google_project.current.project_id
   role   = "roles/compute.admin"
@@ -49,4 +58,29 @@ resource "google_project_iam_member" "service_account_user" {
   project = data.google_project.current.project_id
   role   = "roles/iam.serviceAccountUser"
   member = "serviceAccount:${google_service_account.omnistrate_bootstrap.email}"
+}
+
+## Perms for config connector bootstrap service account
+resource "google_project_iam_member" "cc_compute_admin" {
+    project = data.google_project.current.project_id
+    role   = "roles/compute.admin"
+    member = "serviceAccount:${google_service_account.config_connector_bootstrap.email}"
+}
+
+resource "google_project_iam_member" "cc_security_admin" {
+    project = data.google_project.current.project_id
+    role   = "roles/iam.securityAdmin"
+    member = "serviceAccount:${google_service_account.config_connector_bootstrap.email}"
+}
+
+resource "google_project_iam_member" "cc_service_account_user" {
+    project = data.google_project.current.project_id
+    role   = "roles/iam.serviceAccountUser"
+    member = "serviceAccount:${google_service_account.config_connector_bootstrap.email}"
+}
+
+resource "google_project_iam_member" "cc_storage_admin" {
+    project = data.google_project.current.project_id
+    role   = "roles/storage.admin"
+    member = "serviceAccount:${google_service_account.config_connector_bootstrap.email}"
 }
