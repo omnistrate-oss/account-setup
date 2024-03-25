94a6f983f2a3b10bb90de110b5ae686980c09b69
Tomislav Simeunovic <tomislavs@omnistrate.com>
2024-03-25 12:20:37 -0600

    Connect dataplane agent service account to the workload identity

--- gcp/workload_identity_federation.tf
+++ gcp/workload_identity_federation.tf
@@ -1,5 +1,5 @@
 resource "google_iam_workload_identity_pool" "pool" {
-  project = data.google_project.current.project_id
+  project                   = data.google_project.current.project_id
   workload_identity_pool_id = "omnistrate-bootstrap-id-pool"
   display_name              = "Omnistrate Access Provider Pool"
   description               = "Workfload Identity Pool for the Omnistrate Control Plane"
@@ -7,21 +7,21 @@ resource "google_iam_workload_identity_pool" "pool" {
 }
 
 resource "google_iam_workload_identity_pool_provider" "oidc_provider" {
-  project = data.google_project.current.project_id
-  workload_identity_pool_id = google_iam_workload_identity_pool.pool.workload_identity_pool_id
-  workload_identity_pool_provider_id               = "omnistrate-oidc-prov"
-  display_name              = "Omnistrate OIDC Provider"
-  description               = "OIDC provider for the Omnistrate Control Plane"
-  disabled                  = false
+  project                            = data.google_project.current.project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "omnistrate-oidc-prov"
+  display_name                       = "Omnistrate OIDC Provider"
+  description                        = "OIDC provider for the Omnistrate Control Plane"
+  disabled                           = false
 
   attribute_mapping = {
-    "google.subject" = "assertion.sub"
+    "google.subject"    = "assertion.sub"
     "attribute.subject" = "assertion.sub.extract('subject/{sub}/')"
   }
 
   oidc {
     allowed_audiences = [
-        "sts.amazonaws.com"
+      "sts.amazonaws.com"
     ]
     issuer_uri = "https://oidc.eks.us-west-2.amazonaws.com/id/9AEF0C846C22DEAEFDDD1F98C6AB9FEA"
   }
@@ -39,4 +39,11 @@ resource "google_service_account_iam_member" "config_connector_association" {
   service_account_id = google_service_account.config_connector_bootstrap.id
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${data.google_project.current.project_id}.svc.id.goog[cnrm-system/cnrm-controller-manager]"
+}
+
+# Omnistrate dataplane agent service account connection with the workload identity
+resource "google_service_account_iam_member" "dataplane_service_association" {
+  service_account_id = google_service_account.omnistrate_dataplane_agent.id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${data.google_project.current.project_id}.svc.id.goog[dataplane-agent/omnistrate-da-${var.account_config_identity_id}]"
 }
