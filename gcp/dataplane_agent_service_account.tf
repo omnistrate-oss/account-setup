d4c26c4ba0273d2a7b0c3dc3859dae8178618d38
Tomislav Simeunovic <tomislavs@omnistrate.com>
2024-04-19 11:49:50 -0600

    Create custom role

--- gcp/dataplane_agent_service_account.tf
+++ gcp/dataplane_agent_service_account.tf
@@ -31,8 +31,15 @@ resource "google_project_iam_member" "service_account_user_for_dataplane" {
   member = "serviceAccount:${google_service_account.omnistrate_dataplane_agent.email}"
 }
 
+resource "google_project_iam_custom_role" "omnistrate_custom_iam_sa_policy_manager" {
+  role_id     = "omnistrateServiceAccountPolicyManager"
+  title       = "Omnistrate IAM Service Account Policy Manager (custom role)"
+  description = "Custom role allowing retrieval and manipulation of IAM service account policies from Omnistrate dataplane agent"
+  permissions = ["iam.serviceAccounts.getIamPolicy", "iam.serviceAccounts.setIamPolicy"]
+}
+
 resource "google_project_iam_member" "service_account_manager_for_dataplane" {
   project = data.google_project.current.project_id
-  role   = "roles/composer.ServiceAgentV2Ext"
+  role   = "omnistrateServiceAccountPolicyManager"
   member = "serviceAccount:${google_service_account.omnistrate_dataplane_agent.email}"
 }
