f46cc0cd88fd1cad5afb17f6cd413c4206f26444
Tomislav Simeunovic <tomislavs@omnistrate.com>
2024-09-19 22:14:16 -0600

    Fix boundary

--- aws/iam_policy.tf
+++ aws/iam_policy.tf
@@ -422,6 +422,11 @@ resource "aws_iam_policy" "omnistrate-bootstrap-permissions-boundary" {
         "arn:aws:s3:::omnistrate-${data.aws_caller_identity.current.account_id}-dp-pulumi/*"
       ],
       "Sid": "S3Access"
+    },
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Resource": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/omnistrate-*"
     }
   ],
   "Version": "2012-10-17"
