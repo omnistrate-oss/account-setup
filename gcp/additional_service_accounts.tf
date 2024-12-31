db0e6b7245b411531ceae0a2d38c24146bdde907
Tomislav Simeunovic <tomislavs@omnistrate.com>
2024-12-31 14:32:26 -0700

    Add custom ServiceAccount for Terraform resources

---
+++ gcp/additional_service_accounts.tf
@@ -0,0 +1,7 @@
+# Custom Terraform service account that can be configured with additional permissions
+# and used to create terraform resources.
+resource "google_service_account" "omnistrate_terraform" {
+  project = data.google_project.current.project_id
+  account_id   = "terraform-${lower(var.account_config_identity_id)}"
+  display_name = "Omnistrate Terraform Service Account"
+}
