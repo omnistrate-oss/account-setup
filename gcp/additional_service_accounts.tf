e7bf7315cea22488a5dc97131eb137f6768d3cdb
Tomislav Simeunovic <155485421+tomnislav@users.noreply.github.com>
2025-01-03 11:30:37 -0700

    Shorten service account name (#7)

--- gcp/additional_service_accounts.tf
+++ gcp/additional_service_accounts.tf
@@ -2,6 +2,6 @@
 # and used to create terraform resources.
 resource "google_service_account" "omnistrate_terraform" {
   project = data.google_project.current.project_id
-  account_id   = "omnistrate-terraform-${lower(var.account_config_identity_id)}"
+  account_id   = "omnistrate-tf-${lower(var.account_config_identity_id)}"
   display_name = "Omnistrate Terraform Service Account"
 }
